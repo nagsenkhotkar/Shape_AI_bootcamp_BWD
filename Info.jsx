dc7385d3f7c2531bf210ad0344a9e619f540aea6
nagsenkhotkar <86525215+nagsenkhotkar@users.noreply.github.com>
2021-06-26 20:10:15 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div>
+      <h1>JavaScript and React.js</h1>
+      <p>
+        This is amazing Bootcamp taken by Shaurya Sinha. We Covered everything
+        from Scratch including JavaScript React.js,HTML.
+      </p>
+    </div>
+  );
+}
+export default Info;
